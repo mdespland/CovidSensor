aa943f2579603dcfda6e143f847e427f57ef774a
Marc Despland <marc.despland@orange.com>
2021-07-26 10:51:12 +0200

    Manage complexe payload decoding

--- DecoderAgent/decoder.js
+++ DecoderAgent/decoder.js
@@ -5,11 +5,11 @@ const axios = require('axios');
 
 module.exports = {
     pushDeviceData,
-    decodeDeviceData,
     sendRequest,
     init,
     searchAirQualityObserved,
     checkAirQualityObserved,
+    getAirQualityObserved,
     createAirQualityObserved,
     updateAirQualityObserved,
     createMainSubscription,
@@ -20,15 +20,34 @@ module.exports = {
 
 const Link = "<https://smartdatamodels.org/context.jsonld>; rel=\"http://www.w3.org/ns/json-ld#context\"; type=\"application/ld+json\"";
 
+const MASK_CO2 =            0B00000001
+const MASK_TVOC =           0B00000010
+const MASK_VOLTAGE =        0B00000100
+const MASK_TEMPERATURE =    0B00001000
+const MASK_HUMIDITY =       0B00010000
+const MASK_PRESSURE =       0B00100000
+const MASK_ALTITUDE =       0B01000000
+
+/*
 function decodeDeviceData(data) {
     const buff = Buffer.from(data, 'base64');
     if (buff.length===4) {
         var ppm = buff.readUInt8(0) * 256 + buff.readUInt8(1);
         var volts = buff.readUInt8(2) * 256 + buff.readUInt8(3);
         return [ppm, volts/100]
+    } else {
+        var indice=1;
+        var response=[]
+        if (buff.length>=indice) {
+            if (buff.readUInt8(0) & MASK_CO2===MASK_CO2) {
+                if (buff.length>=indice+2) {
+                    var value=
+                }
+            }
+        }
     }
     return [0, 0];
-}
+}*/
 
 async function createAirQualityObserved(id, refDevice) {
     var now = (new Date()).toISOString()
@@ -38,16 +57,6 @@ async function createAirQualityObserved(id, refDevice) {
         "dateObserved": {
             "type": "Property",
             "value": now
-        },
-        "co2": {
-            "type": "Property",
-            "value": 400,
-            "unitCode": "PPM"
-        },
-        "voltage": {
-            "type": "Property",
-            "value": 0,
-            "unitCode": "V"
         },
         "source": {
             "type": "Property",
@@ -92,11 +101,10 @@ async function searchAirQualityObserved(refDevice) {
 
 async function pushDeviceData(refDevice, data, now) {
     var list = await searchAirQualityObserved(refDevice)
-    var values = decodeDeviceData(data)
-    if (Config.Debug) console.log(JSON.stringify(list, null, 4))
+     if (Config.Debug) console.log(JSON.stringify(list, null, 4))
     for (var i = 0; i < list.length; i++) {
-        console.log(now+"\t Updating "+list[i].id+" with value "+values[0]+" ppm => "+values[1]+" V")
-        await updateAirQualityObserved(list[i].id, values[0], values[1], now)
+        console.log(now + "\t Updating " + list[i].id + " with value " + data)
+        await updateAirQualityObserved(list[i].id, data, now)
     }
 }
 
@@ -110,34 +118,157 @@ async function checkAirQualityObserved(id, refDevice) {
     }
 }
 
-async function updateAirQualityObserved(id, co2, voltage, now) {
-    var attributes = {
+async function getAirQualityObserved(id) {
+    try {
+        var response = await sendRequest("GET", "/entities/" + id, "", "application/ld+json", "application/ld+json", Link)
+        
+        if (response.hasOwnProperty("data")) {
+            if (Config.Debug) console.log(JSON.stringify(response.data, null, 4))
+            return response.data;
+        } else {
+            return Promise.reject("no data in the response : status="+response.status)
+        }
+    } catch (error) {
+        return Promise.reject(error)
+    }
+}
+
+function formatAttribute(value, unit, now) {
+    return {
+        "type": "Property",
+        "value": value,
+        "unitCode": unit,
+        "observedAt": now
+    }
+}
+
+async function updateAirQualityObserved(id, data, now) {
+    if (Config.Debug) console.log("updateAirQualityObserved with data: "+data)
+    const buff = Buffer.from(data, 'base64');
+    var update = {
         "dateObserved": {
             "type": "Property",
             "value": now
-        },
-        "co2": {
-            "type": "Property",
-            "value": co2,
-            "unitCode": "PPM",
-            "observedAt": now
-        },
-        "voltage": {
-            "type": "Property",
-            "value": voltage,
-            "unitCode": "V",
-            "observedAt": now
         },
         "@context": [
             "https://smartdatamodels.org/context.jsonld",
             "https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld"
         ]
     }
+    var create = {
+        "@context": [
+            "https://smartdatamodels.org/context.jsonld",
+            "https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld"
+        ]
+    }
+    var created=false;
     try {
-        var response = await sendRequest("PATCH", "/entities/" + id + "/attrs", attributes, "application/ld+json")
-        return (response.status == 204)
+        var entity = await getAirQualityObserved(id);
+        if (buff.length === 4) {
+            if (entity.hasOwnProperty("co2")) {
+                update.co2=formatAttribute(buff.readUInt8(0) * 256 + buff.readUInt8(1),"59", now)
+            } else {
+                create.co2=formatAttribute(buff.readUInt8(0) * 256 + buff.readUInt8(1),"59", now)
+                created=true;
+            }
+            if (entity.hasOwnProperty("voltage")) {
+                update.voltage=formatAttribute(buff.readUInt8(2) * 256 + buff.readUInt8(3),"VLT", now)
+            } else {
+                create.voltage=formatAttribute(buff.readUInt8(2) * 256 + buff.readUInt8(3),"VLT", now)
+                created=true;
+            }
+
+        } else {
+            if (Config.Debug) console.log("Type of payload "+buff.readUInt8(0) )
+            var indice = 1;
+            if (buff.length > indice) {
+                if ((buff.readUInt8(0) & MASK_CO2) === MASK_CO2) {
+                    if (Config.Debug) console.log("\tHAVE CO2")
+                    if (entity.hasOwnProperty("co2")) {
+                        update.co2=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"59", now)
+                    } else {
+                        create.co2=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"59", now)
+                        created=true;
+                    }
+                    indice += 2;
+                }
+                if ((buff.readUInt8(0) & MASK_TVOC) === MASK_TVOC) {
+                    if (Config.Debug) console.log("\tHAVE TVOC")
+                    if (entity.hasOwnProperty("volatileOrganicCompoundsTotal")) {
+                        update.volatileOrganicCompoundsTotal=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"61", now)
+                    } else {
+                        create.volatileOrganicCompoundsTotal=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"61", now)
+                        created=true;
+                    }
+                    indice += 2;
+                }
+                if ((buff.readUInt8(0) & MASK_VOLTAGE) === MASK_VOLTAGE) {
+                    if (Config.Debug) console.log("\tHAVE VOLTAGE")
+                    if (entity.hasOwnProperty("voltage")) {
+                        update.voltage=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"VLT", now)
+                    } else {
+                        create.voltage=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"VLT", now)
+                        created=true;
+                    }
+                    indice += 2;
+                }
+                if ((buff.readUInt8(0) & MASK_TEMPERATURE)  === MASK_TEMPERATURE ) {
+                    if (Config.Debug) console.log("\tHAVE TEMPERATURE")
+                    if (entity.hasOwnProperty("temperature")) {
+                        update.temperature=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"CEL", now)
+                    } else {
+                        create.temperature=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"CEL", now)
+                        created=true;
+                    }
+                    indice += 2;
+                }
+                if ((buff.readUInt8(0) & MASK_HUMIDITY)  === MASK_HUMIDITY ) {
+                    if (Config.Debug) console.log("\tHAVE HUMIDITY")
+                    if (entity.hasOwnProperty("relativeHumidity")) {
+                        update.relativeHumidity=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"P1", now)
+                    } else {
+                        create.relativeHumidity=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"P1", now)
+                        created=true;
+                    }
+                    indice += 2;
+                }
+                if ((buff.readUInt8(0) & MASK_PRESSURE)  === MASK_PRESSURE ) {
+                    if (Config.Debug) console.log("\tHAVE PRESSURE")
+                    if (entity.hasOwnProperty("pressure")) {
+                        update.pressure=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"A97", now)
+                    } else {
+                        create.pressure=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"A97", now)
+                        created=true;
+                    }
+                    indice += 2;
+                }
+                if ((buff.readUInt8(0) & MASK_ALTITUDE)  === MASK_ALTITUDE ) {
+                    if (Config.Debug) console.log("\tHAVE ALTITUDE")
+                    if (entity.hasOwnProperty("elevation")) {
+                        update.elevation=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"MTR", now)
+                    } else {
+                        create.elevation=formatAttribute(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1),"MTR", now)
+                        created=true;
+                    }
+                    indice += 2;
+                }
+            }
+        }
+        if (created) {
+            try {
+                var response = await sendRequest("POST", "/entities/" + id + "/attrs", create, "application/ld+json")
+            } catch (error) {
+                console.log(JSON.stringify(error))
+            }  
+        }
+        try {
+            var response = await sendRequest("PATCH", "/entities/" + id + "/attrs", update, "application/ld+json")
+            return (response.status == 204)
+        } catch (error) {
+            return Promise.reject(error)
+        }
     } catch (error) {
-        return false;
+        return Promise.reject(error)
     }
 }
 
--- DecoderAgent/test/payload.test.js
+++ DecoderAgent/test/payload.test.js
@@ -7,6 +7,14 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 
+const MASK_CO2 = 0B00000001
+const MASK_TVOC = 0B00000010
+const MASK_VOLTAGE = 0B00000100
+const MASK_TEMPERATURE = 0B00001000
+const MASK_HUMIDITY = 0B00010000
+const MASK_PRESSURE = 0B00100000
+const MASK_ALTITUDE = 0B01000000
+
 describe('Test Lora Payload decodage', function () {
     //this.timeout(10000);
     before(async () => {
@@ -46,5 +54,126 @@ describe('Test Lora Payload decodage', function () {
         expect(volts).to.be.eql(186)
 
 
+    })
+
+
+const MASK_CO2 = 0B00000001
+const MASK_TVOC = 0B00000010
+const MASK_VOLTAGE = 0B00000100
+const MASK_TEMPERATURE = 0B00001000
+const MASK_HUMIDITY = 0B00010000
+const MASK_PRESSURE = 0B00100000
+const MASK_ALTITUDE = 0B01000000
+    it('Encode Lora Payload KQG4ABkEOA==', async () => {
+        var buff=Buffer.allocUnsafe(7);
+        buff.writeUInt8(MASK_CO2 | MASK_TEMPERATURE | MASK_PRESSURE, 0);
+        buff.writeUInt8(Math.floor(440/256),1)
+        buff.writeUInt8(440 % 256,2)
+        buff.writeUInt8(Math.floor(25/256),3)
+        buff.writeUInt8(25 % 256 ,4)
+        buff.writeUInt8(Math.floor(1080/256),5)
+        buff.writeUInt8(1080 % 256,6)
+        expect(buff.readUInt8(0) & MASK_CO2).to.be.eql(MASK_CO2)
+        expect(buff.readUInt8(0) & MASK_TVOC).to.be.eql(0)
+        expect(buff.readUInt8(0) & MASK_VOLTAGE).to.be.eql(0)
+        expect(buff.readUInt8(0) & MASK_TEMPERATURE ).to.be.eql(MASK_TEMPERATURE)
+        expect(buff.readUInt8(0) & MASK_HUMIDITY).to.be.eql(0)
+        expect(buff.readUInt8(0) & MASK_PRESSURE).to.be.eql(MASK_PRESSURE)
+        expect(buff.readUInt8(0) & MASK_ALTITUDE).to.be.eql(0)
+        expect(buff.readUInt8(1) * 256 + buff.readUInt8(2)).to.be.eql(440)
+        expect(buff.readUInt8(3) * 256 + buff.readUInt8(4)).to.be.eql(25)
+        expect(buff.readUInt8(5) * 256 + buff.readUInt8(6)).to.be.eql(1080)
+        expect(buff.toString('base64')).to.be.eql("KQG4ABkEOA==")
+        if ((buff.readUInt8(0) & MASK_CO2) === MASK_CO2) console.log("HAVE CO2")
+        if ((buff.readUInt8(0) & MASK_TVOC) === MASK_TVOC) console.log("HAVE TVOC")
+        if ((buff.readUInt8(0) & MASK_VOLTAGE) === MASK_VOLTAGE) console.log("HAVE VOLTAGE")
+        if ((buff.readUInt8(0) & MASK_TEMPERATURE) === MASK_TEMPERATURE) console.log("HAVE TEMPERATURE")
+        if ((buff.readUInt8(0) & MASK_HUMIDITY) === MASK_HUMIDITY) console.log("HAVE HUMIDITY")
+        if ((buff.readUInt8(0) & MASK_PRESSURE) === MASK_PRESSURE) console.log("HAVE PRESSURE")
+        if ((buff.readUInt8(0) & MASK_ALTITUDE) === MASK_ALTITUDE) console.log("HAVE ALTITUDE")
+
+    })
+
+    it('Encode Lora Payload fwG6ABEHZwAaADgEOgDd', async () => {
+        var buff=Buffer.allocUnsafe(15);
+        buff.writeUInt8(MASK_CO2 | MASK_TVOC | MASK_VOLTAGE | MASK_TEMPERATURE | MASK_HUMIDITY | MASK_PRESSURE | MASK_ALTITUDE, 0);
+        buff.writeUInt8(Math.floor(442/256),1)
+        buff.writeUInt8(442 % 256,2)
+        buff.writeUInt8(Math.floor(17/256),3)
+        buff.writeUInt8(17 % 256 ,4)
+        buff.writeUInt8(Math.floor(1895/256),5)
+        buff.writeUInt8(1895 % 256,6)
+        buff.writeUInt8(Math.floor(26/256),7)
+        buff.writeUInt8(26 % 256,8)
+        buff.writeUInt8(Math.floor(56/256),9)
+        buff.writeUInt8(56 % 256,10)
+        buff.writeUInt8(Math.floor(1082/256),11)
+        buff.writeUInt8(1082 % 256,12)
+        buff.writeUInt8(Math.floor(221/256),13)
+        buff.writeUInt8(221 % 256,14)
+        expect(buff.readUInt8(0) & MASK_CO2).to.be.eql(MASK_CO2)
+        expect(buff.readUInt8(0) & MASK_TVOC).to.be.eql(MASK_TVOC)
+        expect(buff.readUInt8(0) & MASK_VOLTAGE).to.be.eql(MASK_VOLTAGE)
+        expect(buff.readUInt8(0) & MASK_TEMPERATURE ).to.be.eql(MASK_TEMPERATURE)
+        expect(buff.readUInt8(0) & MASK_HUMIDITY).to.be.eql(MASK_HUMIDITY)
+        expect(buff.readUInt8(0) & MASK_PRESSURE).to.be.eql(MASK_PRESSURE)
+        expect(buff.readUInt8(0) & MASK_ALTITUDE).to.be.eql(MASK_ALTITUDE)
+        var indice=1;
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(442)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(17)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(1895)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(26)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(56)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(1082)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(221)
+        indice+=2
+        expect(buff.toString('base64')).to.be.eql("fwG6ABEHZwAaADgEOgDd")
+    })
+    it('Encode Lora Payload fwG9ABYFsAAcAC0CZgAA', async () => {
+        var buff=Buffer.allocUnsafe(15);
+        buff.writeUInt8(MASK_CO2 | MASK_TVOC | MASK_VOLTAGE | MASK_TEMPERATURE | MASK_HUMIDITY | MASK_PRESSURE | MASK_ALTITUDE, 0);
+        buff.writeUInt8(Math.floor(445/256),1)
+        buff.writeUInt8(445 % 256,2)
+        buff.writeUInt8(Math.floor(22/256),3)
+        buff.writeUInt8(22 % 256 ,4)
+        buff.writeUInt8(Math.floor(1456/256),5)
+        buff.writeUInt8(1456 % 256,6)
+        buff.writeUInt8(Math.floor(28/256),7)
+        buff.writeUInt8(28 % 256,8)
+        buff.writeUInt8(Math.floor(45/256),9)
+        buff.writeUInt8(45 % 256,10)
+        buff.writeUInt8(Math.floor(614/256),11)
+        buff.writeUInt8(614 % 256,12)
+        buff.writeUInt8(Math.floor(0/256),13)
+        buff.writeUInt8(0 % 256,14)
+        expect(buff.readUInt8(0) & MASK_CO2).to.be.eql(MASK_CO2)
+        expect(buff.readUInt8(0) & MASK_TVOC).to.be.eql(MASK_TVOC)
+        expect(buff.readUInt8(0) & MASK_VOLTAGE).to.be.eql(MASK_VOLTAGE)
+        expect(buff.readUInt8(0) & MASK_TEMPERATURE ).to.be.eql(MASK_TEMPERATURE)
+        expect(buff.readUInt8(0) & MASK_HUMIDITY).to.be.eql(MASK_HUMIDITY)
+        expect(buff.readUInt8(0) & MASK_PRESSURE).to.be.eql(MASK_PRESSURE)
+        expect(buff.readUInt8(0) & MASK_ALTITUDE).to.be.eql(MASK_ALTITUDE)
+        var indice=1;
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(445)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(22)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(1456)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(28)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(45)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(614)
+        indice+=2
+        expect(buff.readUInt8(indice) * 256 + buff.readUInt8(indice+1)).to.be.eql(0)
+        indice+=2
+        expect(buff.toString('base64')).to.be.eql("fwG9ABYFsAAcAC0CZgAA")
     })
 })
